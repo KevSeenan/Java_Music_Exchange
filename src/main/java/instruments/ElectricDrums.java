62abd823ccfccf26425237f209e181024fa08342
KevSeenan <kev.seenan@gmail.com>
2019-10-04 13:00:07 +0100

    Adds tests for electric drums

--- src/main/java/instruments/ElectricDrums.java
+++ src/main/java/instruments/ElectricDrums.java
@@ -1,4 +1,15 @@
 package instruments;
 
-public class ElectricDrums {
+import behaviours.IPlay;
+
+public class ElectricDrums extends Drums implements IPlay {
+
+    public ElectricDrums(String make, String model) {
+
+        super(make, model);
+    }
+
+    public String playInstrument(String string) {
+        return "The electric drums are playing " + string;
+    }
 }
--- src/test/java/instruments/ElectricDrumsTest.java
+++ src/test/java/instruments/ElectricDrumsTest.java
@@ -1,4 +1,34 @@
 package instruments;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class ElectricDrumsTest {
+
+    ElectricDrums  electricDrums;
+
+    @Before
+    public void ElectricDrums() {
+        electricDrums = new ElectricDrums ("Yamaha","DTX402");
+    }
+
+    @Test
+    public void hasAMake() {
+        assertEquals("Yamaha", electricDrums.getMake());
+    }
+
+    @Test
+    public void hasAModel() {
+        assertEquals("DTX402", electricDrums.getModel());
+    }
+
+    @Test
+    public void canBePlayed() {
+        assertNotNull(electricDrums);
+        assertEquals("The electric drums are playing Enjoy The Silence by Depeche Mode",
+                electricDrums.playInstrument("Enjoy The Silence by Depeche Mode"));
+    }
 }
