fe7a6169eeac481283809a7b606bb07e645ba373
KevSeenan <kev.seenan@gmail.com>
2019-10-04 12:37:46 +0100

    Adds tests for electro acoustic guitar

--- src/main/java/instruments/ElectroAcousticGuitar.java
+++ src/main/java/instruments/ElectroAcousticGuitar.java
@@ -2,5 +2,14 @@ package instruments;
 
 import behaviours.IPlay;
 
-public class ElectroAcousticGuitar {
+public class ElectroAcousticGuitar extends Guitars implements IPlay {
+
+    public ElectroAcousticGuitar(String make, String model, int numberOfStrings, int numberOfPickUps) {
+
+        super(make, model, numberOfStrings, numberOfPickUps);
+    }
+
+    public String playInstrument(String string) {
+        return "The electro acoustic guitar is playing " + string;
+    }
 }
--- src/test/java/instruments/ElectroAcousticGuitarTest.java
+++ src/test/java/instruments/ElectroAcousticGuitarTest.java
@@ -1,4 +1,47 @@
 package instruments;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class ElectroAcousticGuitarTest {
+
+    ElectroAcousticGuitar  electroAcousticGuitar;
+
+    @Before
+    public void ElectroAcousticGuitar() {
+        electroAcousticGuitar = new ElectroAcousticGuitar ("Yamaha",
+                "APX700II",
+                6,
+                1);
+    }
+
+    @Test
+    public void hasAMake() {
+        assertEquals("Yamaha", electroAcousticGuitar.getMake());
+    }
+
+    @Test
+    public void hasAModel() {
+        assertEquals("APX700II", electroAcousticGuitar.getModel());
+    }
+
+    @Test
+    public void hasStrings() {
+        assertEquals(6, electroAcousticGuitar.getNumberOfStrings());
+    }
+
+    @Test
+    public void hasPickUps() {
+        assertEquals(1, electroAcousticGuitar.getNumberOfPickUps());
+    }
+
+    @Test
+    public void canBePlayed() {
+        assertNotNull(electroAcousticGuitar);
+        assertEquals("The electro acoustic guitar is playing Ghost Riders in The Sky by Johnny Cash",
+                electroAcousticGuitar.playInstrument("Ghost Riders in The Sky by Johnny Cash"));
+    }
 }
