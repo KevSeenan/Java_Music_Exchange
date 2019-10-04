b3ea029d0e5c6bac52e3cfa67a7aa73b0c3fd17a
KevSeenan <kev.seenan@gmail.com>
2019-10-04 13:29:42 +0100

    Adds tests for keyboard

--- src/main/java/instruments/Keyboard.java
+++ src/main/java/instruments/Keyboard.java
@@ -1,7 +1,28 @@
-//package instruments;
-//
-//import behaviours.IPlay;
-//
-//public class Keyboard implements IPlay {
-//
-//}
+package instruments;
+
+import behaviours.IPlay;
+
+public class Keyboard implements IPlay {
+
+    private String make;
+    private String model;
+
+
+    public Keyboard(String make, String model) {
+
+        this.make = make;
+        this.model = model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String playInstrument(String string) {
+        return "The keyboard is playing " + string;
+    }
+}
--- src/test/java/instruments/KeyboardTest.java
+++ src/test/java/instruments/KeyboardTest.java
@@ -1,4 +1,34 @@
 package instruments;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class KeyboardTest {
+
+    Keyboard  keyboard;
+
+    @Before
+    public void AcousticDrum() {
+        keyboard = new Keyboard ("Casio","CTK 1500");
+    }
+
+    @Test
+    public void hasAMake() {
+        assertEquals("Casio", keyboard.getMake());
+    }
+
+    @Test
+    public void hasAModel() {
+        assertEquals("CTK 1500", keyboard.getModel());
+    }
+
+    @Test
+    public void canBePlayed() {
+        assertNotNull(keyboard);
+        assertEquals("The keyboard is playing Chopsticks",
+                keyboard.playInstrument("Chopsticks"));
+    }
 }
