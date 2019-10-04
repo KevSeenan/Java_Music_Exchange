f829525ca9dcdb426a0033bb7008b605bcfba8ba
KevSeenan <kev.seenan@gmail.com>
2019-10-04 12:27:05 +0100

    Adds tests for electric guitar

--- src/main/java/instruments/ElectricGuitar.java
+++ src/main/java/instruments/ElectricGuitar.java
@@ -1,4 +1,15 @@
 package instruments;
 
-public class ElectricGuitar {
+import behaviours.IPlay;
+
+public class ElectricGuitar extends Guitars implements IPlay {
+
+    public ElectricGuitar(String make, String model, int numberOfStrings, int numberOfPickUps) {
+
+        super(make, model, numberOfStrings, numberOfPickUps);
+    }
+
+    public String playInstrument(String string) {
+        return "The electric guitar is playing " + string;
+    }
 }
--- src/test/java/instruments/ElectricGuitarTest.java
+++ src/test/java/instruments/ElectricGuitarTest.java
@@ -1,4 +1,47 @@
 package instruments;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class ElectricGuitarTest {
+
+    ElectricGuitar  electricGuitar;
+
+    @Before
+    public void AcousticGuitar() {
+        electricGuitar = new ElectricGuitar ("Gibson",
+                "Les Paul Special",
+                6,
+                2);
+    }
+
+    @Test
+    public void hasAMake() {
+        assertEquals("Gibson", electricGuitar.getMake());
+    }
+
+    @Test
+    public void hasAModel() {
+        assertEquals("Les Paul Special", electricGuitar.getModel());
+    }
+
+    @Test
+    public void hasStrings() {
+        assertEquals(6, electricGuitar.getNumberOfStrings());
+    }
+
+    @Test
+    public void hasPickUps() {
+        assertEquals(2, electricGuitar.getNumberOfPickUps());
+    }
+
+    @Test
+    public void canBePlayed() {
+        assertNotNull(electricGuitar);
+        assertEquals("The electric guitar is playing Paradise City by Guns N' Roses",
+                electricGuitar.playInstrument("Paradise City by Guns N' Roses"));
+    }
 }
